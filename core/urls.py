3c14b49ec95ef4126f0e72be1122412b0b25274c
dancoon <dancun.ian@students.jkuat.ac.ke>
2023-01-07 22:45:07 +0300

    Updates

--- core/urls.py
+++ core/urls.py
@@ -17,6 +17,9 @@ urlpatterns = [
     path('owner/', views.display_admin_account, name='display_admin_account'),
     path('owner/tenantsinfo/', views.owner_tenants_info, name='owner_tenants_info'),
     path('owner/roomsinfo/', views.owner_rooms_info, name='owner_rooms_info'),
+    
+    path('owner/rents/', views.view_tenants_pay, name='view_tenants_pay'),
+
     path('owner/enrolltenantsview/', views.enroll_tenants_view, name='enroll_tenants_view'),
     path('owner/enrolltenants/ <int:pk>', views.enroll_tenants, name='enroll_tenants'),
 ]
